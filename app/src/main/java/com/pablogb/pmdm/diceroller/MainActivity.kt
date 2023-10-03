8f81be65b7e2a534db25eedf7d2d74b49d2dc5d3
Pablo Gonzalez del Barrio <pablogb69@educastur.es>
2023-10-03 16:26:47 +0200

    FASE 3

--- app/src/main/java/com/pablogb/pmdm/diceroller/MainActivity.kt
+++ app/src/main/java/com/pablogb/pmdm/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pablogb.pmdm.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,19 +15,53 @@ class MainActivity : AppCompatActivity() {
         val  rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener{
             rollDice()
+            rollDice2()
         }
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resulTextView: TextView = findViewById(R.id.textView)
-        resulTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        /*when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+        */
+
+        val drawableResource = when(diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
+    private fun rollDice2() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+
+        when (diceRoll) {
+            1 -> diceImage2.setImageResource(R.drawable.dice_1)
+            2 -> diceImage2.setImageResource(R.drawable.dice_2)
+            3 -> diceImage2.setImageResource(R.drawable.dice_3)
+            4 -> diceImage2.setImageResource(R.drawable.dice_4)
+            5 -> diceImage2.setImageResource(R.drawable.dice_5)
+            6 -> diceImage2.setImageResource(R.drawable.dice_6)
+        }
 }
 
-class Dice (val numSides: Int){
+class Dice (val numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
+        }
     }
 }
