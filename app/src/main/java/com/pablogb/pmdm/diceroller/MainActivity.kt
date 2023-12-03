c43fe844df6a7c6314db4f8cafbb6b831e5dd9a2
Pablo <pabloglezdelbarrio@gmail.com>
2023-12-03 19:34:00 +0100

    final

--- app/src/main/java/com/pablogb/pmdm/diceroller/MainActivity.kt
+++ app/src/main/java/com/pablogb/pmdm/diceroller/MainActivity.kt
@@ -4,15 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val  rollButton: Button = findViewById(R.id.button)
+        val  rollButton: Button = findViewById(R.id.btnRoll)
         rollButton.setOnClickListener{
             rollDice()
             rollDice2()
@@ -23,7 +21,6 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val diceImage: ImageView = findViewById(R.id.imageView)
-        val diceImage2: ImageView = findViewById(R.id.imageView2)
         /*when (diceRoll) {
             1 -> diceImage.setImageResource(R.drawable.dice_1)
             2 -> diceImage.setImageResource(R.drawable.dice_2)
---
+++ app/src/main/java/com/pablogb/pmdm/rgbcolors/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pablogb.pmdm.rgbcolors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pablogb.pmdm.diceroller.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
